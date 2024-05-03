690c60ce2e401f408729677973790d2ed350817f
dkuku <daniel.kukula@fresha.com>
2024-05-03 13:25:05 +0100

    update checks to separate functions

--- lib/unleash/strategy/constraint.ex
+++ lib/unleash/strategy/constraint.ex
@@ -46,8 +46,8 @@ defmodule Unleash.Strategy.Constraint do
 
   defp check(val, "DATE_AFTER", %{"value" => value}), do: compare_date(val, value, :gt)
   defp check(val, "DATE_BEFORE", %{"value" => value}), do: compare_date(val, value, :lt)
-  defp check(val, "IN", %{"values" => values}), do: val in values
-  defp check(val, "NOT_IN", %{"values" => values}), do: val not in values
+  defp check(val, "IN", %{"values" => values}), do: compare_in(val, values, :in)
+  defp check(val, "NOT_IN", %{"values" => values}), do: compare_in(val, values, :not_in)
   defp check(val, "NUM_EQ", %{"value" => value}), do: compare_num(val, value, :eq)
   defp check(val, "NUM_GT", %{"value" => value}), do: compare_num(val, value, :gt)
   defp check(val, "NUM_GTE", %{"value" => value}), do: compare_num(val, value, :gte)
@@ -56,10 +56,35 @@ defmodule Unleash.Strategy.Constraint do
   defp check(val, "SEMVER_EQ", %{"value" => value}), do: compare_semver(val, value, :eq)
   defp check(val, "SEMVER_GT", %{"value" => value}), do: compare_semver(val, value, :gt)
   defp check(val, "SEMVER_LT", %{"value" => value}), do: compare_semver(val, value, :lt)
-  defp check(val, "STR_CONTAINS", %{"values" => values}), do: Enum.any?(values, &String.contains?(val, &1))
-  defp check(val, "STR_ENDS_WITH", %{"values" => values}), do: Enum.any?(values, &String.ends_with?(val, &1))
-  defp check(val, "STR_STARTS_WITH", %{"values" => values}), do: Enum.any?(values, &String.starts_with?(val, &1))
+  defp check(val, "STR_CONTAINS", %{"values" => values}), do: compare_str(val, values, :contains)
+  defp check(val, "STR_ENDS_WITH", %{"values" => values}), do: compare_str(val, values, :ends)
+  defp check(val, "STR_STARTS_WITH", %{"values" => values}), do: compare_str(val, values, :starts)
   defp check(_val, _, _), do: false
+
+  defp compare_in(val, values, equality) when is_integer(val) do
+    compare_in(to_string(val), values, equality)
+  end
+
+  defp compare_in(val, values, equality) when is_list(values) do
+    case equality do
+      :in -> val in values
+      :not_in -> val not in values
+    end
+  end
+
+  defp compare_in(_val, _values, _equality), do: false
+
+  defp compare_str(val, values, equality) when is_binary(val) and is_list(values) do
+    Enum.any?(values, fn value ->
+      case equality do
+        :contains -> String.contains?(val, value)
+        :ends -> String.ends_with?(val, value)
+        :starts -> String.starts_with?(val, value)
+      end
+    end)
+  end
+
+  defp compare_str(_val, _values, _equality), do: false
 
   defp compare_semver(v1, v2, equality) when is_binary(v1) and is_binary(v2) do
     with {:ok, v1} <- Version.parse(v1),
